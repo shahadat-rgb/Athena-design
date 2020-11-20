37889bc432ca7737e5f1a64315657595a3051f8c
Emdadul-Tareque <emdadultareque@gmail.com>
2020-11-20 18:39:14 +0600

    footter section and team section added by tareque

--- src/Components/Home/LastThreeSection/LastThreeSection.js
+++ src/Components/Home/LastThreeSection/LastThreeSection.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Achievements from '../Achievements/Achievements';
+import DedicatedTeam from '../DedicatedTeam/DedicatedTeam';
+import Footer from '../Footer/Footer';
 
 
 const LastThreeSection = () => {
     return (  
         <div className='work-background'>
             <Achievements></Achievements>
+            <DedicatedTeam></DedicatedTeam>
+            <Footer></Footer>
         </div>
         
     );
